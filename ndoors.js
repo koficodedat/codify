bc5849fe1ff2eb0cedd2694d82aaa6250b5e8980
koficodedat <koficodedat@gmail.com>
2018-06-09 13:34:05 -0400

    added ndoors: verify which doors are still open after making n passes n times. will add description in README

---
+++ ndoors.js
@@ -0,0 +1,62 @@
+(
+    () => {
+        const numberOfDoors = process.argv.slice(2)[0];
+
+        if( !numberOfDoors ) throw Error('Argument missing: How many number of doors?');
+        if( isNaN( numberOfDoors ) ) throw Error('Wrong datatype: Argument must be a number');
+
+        let doors = Array( parseInt( numberOfDoors ) ).fill( false ) //initially closed
+        let leap = 1;
+
+        run = () => {
+
+            while( leap <= numberOfDoors ){
+                cycle();
+                updateLeap();
+            }
+
+            console.log( 
+                'open doors: ', 
+                doors.map( 
+                    (door, index) => {
+                        if( door ) return index;
+                    }
+                )
+                .filter( doorNumber => doorNumber )
+            );
+
+            console.log( `closed doors: every door between 0 and ${numberOfDoors} which is not in the open door list` );
+        }
+
+        cycle = () => {
+
+            let index = 0;
+            while( index <= numberOfDoors ){
+                toggle( index );
+                index += leap;
+            }
+
+        }
+
+        toggle = (index) => {
+            doors[ index ] = !doors[ index ];
+        }
+
+        updateLeap = () => {
+            leap += 1;
+        }
+
+        performanceRun = () => {
+            // console.timeStamp(`TESTING ${numberOfDoors} DOORS`);
+            let start = Date.now();
+            run()
+            // console.timeStamp(`TESTING ${numberOfDoors} DOORS`);
+            console.log( (Date.now() - start) / 1000 );
+        }
+
+        //run main function
+        performanceRun();
+
+
+    }
+)()
